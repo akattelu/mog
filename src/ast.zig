36801ebbf23a79498804a0d36737a88baab45fbd
Aakash Kattelu <aakash.kattelu@gmail.com>
2024-12-08 14:14:22 -0800

    Add string writer methods

--- src/ast.zig
+++ src/ast.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const token = @import("token.zig");
+const AnyWriter = std.io.AnyWriter;
 
 pub const NodeType = enum { Statement, Expression, Program };
 
@@ -15,16 +16,33 @@ pub const Node = union(NodeType) {
             .Program => |n| return n.tokenLiteral(),
         }
     }
+
+    pub fn string(self: *const Node, writer: AnyWriter) !void {
+        switch (self.*) {
+            .Statement => |n| try n.string(writer),
+            .Expression => |n| try n.string(writer),
+            .Program => |n| try n.string(writer),
+        }
+    }
 };
 
-pub const StatementTypes = enum { Let, Return };
+pub const StatementTypes = enum { Let, Return, Expression };
 pub const Statement = union(StatementTypes) {
     Let: *LetStatement,
     Return: *ReturnStatement,
+    Expression: *ExpressionStatement,
     pub fn tokenLiteral(self: *const Statement) []const u8 {
         switch (self.*) {
             .Let => |n| return n.tokenLiteral(),
             .Return => |n| return n.tokenLiteral(),
+            .Expression => |n| return n.tokenLiteral(),
+        }
+    }
+    pub fn string(self: *const Statement, writer: AnyWriter) !void {
+        switch (self.*) {
+            .Let => |n| try n.string(writer),
+            .Return => |n| try n.string(writer),
+            .Expression => |n| try n.string(writer),
         }
     }
 };
@@ -37,6 +55,14 @@ pub const ReturnStatement = struct {
     pub fn tokenLiteral(self: *const ReturnStatement) []const u8 {
         return self.token.literal;
     }
+
+    pub fn string(self: *const ReturnStatement, writer: AnyWriter) !void {
+        try writer.write("return ");
+        if (self.expr != null) {
+            try self.expr.?.string(writer);
+        }
+        try writer.write(";");
+    }
 };
 
 pub const LetStatement = struct {
@@ -46,6 +72,27 @@ pub const LetStatement = struct {
     expr: ?*Expression,
     pub fn tokenLiteral(self: *const LetStatement) []const u8 {
         return self.token.literal;
+    }
+    pub fn string(self: *const LetStatement, writer: AnyWriter) !void {
+        try writer.write("let ");
+        try self.name.string(writer);
+        try writer.write(" = ");
+        if (self.expr != null) {
+            try self.expr.?.string(writer);
+        }
+        try writer.write(";");
+    }
+};
+
+pub const ExpressionStatement = struct {
+    token: token.Token,
+    expr: *Expression,
+
+    pub fn tokenLiteral(self: *const ExpressionStatement) []const u8 {
+        return self.token.literal;
+    }
+    pub fn string(self: *const ExpressionStatement, writer: AnyWriter) !void {
+        try self.expr.string(writer);
     }
 };
 
@@ -59,6 +106,11 @@ pub const Expression = union(ExpressionTypes) {
             .Identifier => |n| return n.tokenLiteral(),
         }
     }
+    pub fn string(self: *const Statement, writer: AnyWriter) !void {
+        switch (self.*) {
+            .Identifier => |n| try n.string(writer),
+        }
+    }
 };
 
 pub const Identifier = struct {
@@ -66,6 +118,9 @@ pub const Identifier = struct {
     value: []const u8,
     pub fn tokenLiteral(self: *const Identifier) []const u8 {
         return self.token.literal;
+    }
+    pub fn string(self: *const Identifier, writer: AnyWriter) !void {
+        try writer.write(self.value);
     }
 };
 
@@ -77,6 +132,12 @@ pub const Program = struct {
             return self.statements[0].tokenLiteral();
         } else {
             return "";
+        }
+    }
+    pub fn string(self: *const Program, writer: AnyWriter) !void {
+        for (self.statements) |stmt| {
+            try stmt.string(writer);
+            try stmt.string("\n");
         }
     }
 };
